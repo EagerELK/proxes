902c5ac30e0a878956aa337bfa6112c218e309a3
Jurgens du Toit <jrgns@jadeit.co.za>
2016-12-20 20:50:31 +0200

    chore: Add Code Climate integration to Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 ENV['RACK_ENV'] ||= 'test'
 
+require "simplecov"
+SimpleCov.start
+
 require 'proxes'
 require 'proxes/db'
 if ENV['DATABASE_URL'] == 'sqlite::memory:'
