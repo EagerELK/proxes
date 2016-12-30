d1dadc67eac73053ad765bf42e96648747c15768
Jurgens du Toit <jrgns@jadeit.co.za>
2016-12-30 23:00:54 +0200

    refac: Clean up the React dashboard

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ProxesComponents from 'react-proxes-components/react-proxes-components';
 
-ReactDOM.render(
-    <ProxesComponents autoPoll="true" pollInterval="200000"/>,
-    document.getElementById('indexlist')
-);
+ReactDOM.render(<ProxesComponents pollInterval="30000"/>, document.getElementById('react-dashboard'));
+
+// ReactDOM.render(
+//     <Health store={new ESStore()}/>,
+//     document.getElementById('indexlist')
+// );
