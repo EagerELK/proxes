857a0882b85515b0026bf87646bdeb566213bd66
Sergey Shkarupa <s.shkarupa@gmail.com>
2017-01-14 20:30:02 +0100

    fix: Change ProxES::Loggers::Elasticsearch to an empty class that extends Logger

--- lib/proxes/loggers/elacticsearch.rb
+++ lib/proxes/loggers/elacticsearch.rb
@@ -1,16 +1,9 @@
 # frozen_string_literal: true
+require 'logger'
+
 module ProxES
   module Loggers
-    class Elasticsearch
-      attr_accessor :level, :url, :log
-
-      WARN = 2
-
-      def initialize(args)
-        @level = args['level'] || 0
-        @url = args['url'] || ''
-        @log = args['log'] || false
-      end
+    class Elasticsearch < ::Logger
     end
   end
 end
