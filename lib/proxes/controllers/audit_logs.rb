c3f695d42fd015a35fb3677a07a9dd06c9349fea
Sergey Shkarupa <s.shkarupa@gmail.com>
2017-01-19 13:13:18 +0100

    fix: Update routes for show only list of audit logs

--- lib/proxes/controllers/audit_logs.rb
+++ lib/proxes/controllers/audit_logs.rb
@@ -1,16 +1,32 @@
 # frozen_string_literal: true
-require 'proxes/controllers/component'
+require 'proxes/controllers/application'
+require 'proxes/helpers/component'
 require 'proxes/models/audit_log'
 require 'proxes/policies/audit_log_policy'
 
 module ProxES
-  class AuditLogs < Component
+  class AuditLogs < Application
+    helpers ProxES::Helpers::Component
     set model_class: ProxES::AuditLog
-    set view_location: 'audit_logs'
-    set base_path: '/_proxes/audit_logs'
 
-    get '/' do
-      halt 404
+   # List
+    get '/', provides: [:html, :json] do
+      authorize settings.model_class, :list
+
+      respond_to do |format|
+        format.html do
+          haml :"audit_logs/index",
+            locals: { list: list, title: heading(:list) }
+        end
+        format.json do
+          {
+            'items' => list.map { |entity| entity.values },
+            'page' => params[:page],
+            'count' => params[:count],
+            'total' => list.to_a.size
+          }.to_json
+        end
+      end
     end
   end
 end
