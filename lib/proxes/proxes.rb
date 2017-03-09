60a459c4bfb943a236abba51edd049fd4edee8a4
Jurgens du Toit <jrgns@automationexchange.co.za>
2017-03-09 23:07:10 +0200

    chore: Define plugin routes and nav items

--- lib/proxes/proxes.rb
+++ lib/proxes/proxes.rb
@@ -1,10 +1,33 @@
 # frozen_string_literal: true
 require 'proxes'
+require 'proxes/db'
+require 'proxes/controllers/users'
+require 'proxes/controllers/roles'
+require 'proxes/controllers/permissions'
+require 'proxes/controllers/audit_logs'
 
 module ProxES
   class ProxES
     def self.migration_folder
       File.expand_path('../../../migrate', __FILE__)
+    end
+
+    def self.route_mappings
+      {
+        '/' => App,
+        '/users' => Users,
+        '/roles' => Roles,
+        '/permissions' => Permissions,
+        '/audit-logs' => AuditLogs,
+      }
+    end
+
+    def self.nav_items
+      [
+        { order: 0, link:'/users/', text: 'Users' },
+        { order: 1, link:'/roles/', text: 'Roles' },
+        { order: 2, link:'/permissions/', text: 'Permissions' },
+      ]
     end
   end
 end
