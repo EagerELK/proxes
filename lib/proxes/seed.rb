f6cfb4f8b80e706bf56c5d218969ce23af84a2b8
Jurgens du Toit <jrgns@automationexchange.co.za>
2017-03-25 10:45:52 +0200

    fix: Add INDEX to seed permissions

--- lib/proxes/seed.rb
+++ lib/proxes/seed.rb
@@ -4,6 +4,6 @@ require 'proxes/models/permission'
 
 ProxES::Role.find_or_create(name: 'user')
 sa = ProxES::Role.find_or_create(name: 'super_admin')
-%w(GET POST PUT DELETE HEAD OPTIONS).each do |verb|
+%w(GET POST PUT DELETE HEAD OPTIONS INDEX).each do |verb|
   ProxES::Permission.find_or_create(role: sa, verb: verb, pattern: '.*')
 end
