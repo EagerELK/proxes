8656a1e7f5c2053ed76efa20577b608d3d5c9805
Jurgens du Toit <jrgns@automationexchange.co.za>
2017-05-10 21:46:56 +0200

    feat: Ability for plugins to provide seeders

--- lib/proxes/seed.rb
+++ lib/proxes/seed.rb
@@ -2,8 +2,6 @@ require 'proxes/db'
 require 'proxes/models/role'
 require 'proxes/models/permission'
 
-ProxES::Role.find_or_create(name: 'user')
-sa = ProxES::Role.find_or_create(name: 'super_admin')
-%w(GET POST PUT DELETE HEAD OPTIONS INDEX).each do |verb|
-  ProxES::Permission.find_or_create(role: sa, verb: verb, pattern: '.*')
+::ProxES::Container.seeders.each do |seeder|
+  seeder.call
 end
