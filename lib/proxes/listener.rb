1325f9f410337015981093c242c5e924cefc27fe
Jurgens du Toit <jrgns@automationexchange.co.za>
2017-05-10 21:44:43 +0200

    fix: Synchronize audit log creation

--- lib/proxes/listener.rb
+++ lib/proxes/listener.rb
@@ -2,11 +2,15 @@ require 'wisper'
 
 module ProxES
   class Listener
+    def initialize
+      @mutex = Mutex.new
+    end
+
     def method_missing(method, *args, &block)
       vals = { action: method }
       vals[:user] = args[0][:user] if (args[0] && args[0].has_key?(:user))
       vals[:details] = args[0][:details] if (args[0] && args[0].has_key?(:details))
-      AuditLog.create vals
+      @mutex.synchronize { AuditLog.create vals }
     end
 
     def respond_to_missing?(method, include_private = false)
