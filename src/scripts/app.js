8ae1fa6277c20d11feeffa3e54632cd407744b10
Jurgens du Toit <jrgns@automationexchange.co.za>
2017-05-16 23:14:26 +0200

    fix: Make react component ES url configurable from HTML

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ProxesComponents from 'react-proxes-components/react-proxes-components';
 
-ReactDOM.render(<ProxesComponents pollInterval="30000"/>, document.getElementById('react-dashboard'));
+var elasticsearch_url = document.getElementById('react-dashboard').getAttribute('data-elasticsearch-url');
+ReactDOM.render(<ProxesComponents pollInterval="30000" elasticsearch_url={elasticsearch_url}/>, document.getElementById('react-dashboard'));
 
 // ReactDOM.render(
 //     <Health store={new ESStore()}/>,
