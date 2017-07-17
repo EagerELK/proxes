4d744bad24beddfdfacc23f1cf9a086a17cf095a
Jurgens du Toit <jrgns@jadeit.co.za>
2017-07-17 23:10:49 +0200

    fix: Make Sequel log level configurable

--- lib/proxes/db.rb
+++ lib/proxes/db.rb
@@ -7,6 +7,8 @@ require 'proxes/services/logger'
 # passed to subprocesses.  DATABASE_URL may contain passwords.
 DB = Sequel.connect(ENV['RACK_ENV'] == 'production' ? ENV.delete('DATABASE_URL') : ENV['DATABASE_URL'])
 
+log_level = (ENV['SEQUEL_LOGGING_LEVEL'] || :debug).to_sym
+DB.sql_log_level = log_level
 DB.loggers << ProxES::Services::Logger.instance
 
 DB.extension(:pagination)
