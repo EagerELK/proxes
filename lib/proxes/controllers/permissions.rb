4f5ddd6dfab80ecb6db769178813245d056ccd25
Jurgens du Toit <jrgns@jadeit.co.za>
2017-10-30 22:50:43 +0200

    feat: Search and filters on permissions

--- lib/proxes/controllers/permissions.rb
+++ lib/proxes/controllers/permissions.rb
@@ -8,6 +8,28 @@ module ProxES
   class Permissions < Ditty::Component
     set model_class: Permission
 
+    FILTERS = [
+      { name: :user, field: 'user.email' },
+      { name: :role, field: 'role.name' },
+      { name: :verb }
+    ].freeze
+
+    SEARCHABLE = %i[pattern]
+
+    helpers do
+      def user_options
+        policy_scope(::Ditty::User).as_hash(:email, :email)
+      end
+
+      def role_options
+        policy_scope(::Ditty::Role).as_hash(:name, :name)
+      end
+
+      def verb_options
+        ProxES::Permission.verbs
+      end
+    end
+
     def find_template(views, name, engine, &block)
       super(views, name, engine, &block) # Root
       super(::Ditty::ProxES.view_folder, name, engine, &block) # This Component
