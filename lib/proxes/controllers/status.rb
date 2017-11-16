eb22358dc5fff442f761764e8d7080fd2552a1a4
Jurgens du Toit <jrgns@jadeit.co.za>
2017-11-16 22:17:12 +0200

    fix: Handle cluster unavailable in status check

--- lib/proxes/controllers/status.rb
+++ lib/proxes/controllers/status.rb
@@ -23,46 +23,45 @@ module ProxES
         health = cluster_health
         checks << { text: 'Cluster Reachable', passed: true, value: health['cluster_name'] }
         checks << { text: 'Cluster Health', passed: health['status'] == 'green', value: health['status'] }
+
+        node_stats = client.nodes.stats metric: 'os,fs,jvm'
+
+        jvm_values = []
+        jvm_passed = true
+        node_stats['nodes'].each do |name, node|
+          jvm_values << "#{name}: #{node['jvm']['mem']['heap_used_percent']}%"
+          jvm_passed = false if node['jvm']['mem']['heap_used_percent'] > 85
+        end
+        checks << { text: 'Node JVM Heap', passed: jvm_passed, value: jvm_values }
+
+        fs_values = []
+        fs_passed = true
+        node_stats['nodes'].each do |name, node|
+          stats = node['fs']['total']
+          left = stats['available_in_bytes'] / stats['total_in_bytes'].to_f * 100
+          fs_values << "#{name}: #{'%.02f' % left}% Free"
+          fs_passed = false if left < 10
+        end
+        checks << { text: 'Node File Systems', passed: fs_passed, value: fs_values }
+
+        cpu_values = []
+        cpu_passed = true
+        node_stats['nodes'].each do |name, node|
+          cpu_values << "#{name}: #{node['os']['cpu']['percent']}"
+          cpu_passed = false if node['os']['cpu']['percent'].to_i > 70
+        end
+        checks << { text: 'Node CPU Usage', passed: cpu_passed, value: cpu_values }
+
+        memory_values = []
+        memory_passed = true
+        node_stats['nodes'].each do |name, node|
+          memory_values << "#{name}: #{node['os']['mem']['used_percent']}"
+          memory_passed = false if node['os']['mem']['used_percent'].to_i >= 100
+        end
+        checks << { text: 'Node Memory Usage', passed: memory_passed, value: memory_values }
       rescue StandardError
         checks << { text: 'Cluster Reachable', passed: false}
       end
-
-      node_stats = client.nodes.stats metric: 'os,fs,jvm'
-
-      jvm_values = []
-      jvm_passed = true
-      node_stats['nodes'].each do |name, node|
-        jvm_values << "#{name}: #{node['jvm']['mem']['heap_used_percent']}%"
-        jvm_passed = false if node['jvm']['mem']['heap_used_percent'] > 85
-      end
-      checks << { text: 'Node JVM Heap', passed: jvm_passed, value: jvm_values }
-
-      fs_values = []
-      fs_passed = true
-      node_stats['nodes'].each do |name, node|
-        next if node['attributes']['data'] == 'false'
-        stats = node['fs']['total']
-        left = stats['available_in_bytes'] / stats['total_in_bytes'].to_f * 100
-        fs_values << "#{name}: #{'%.02f' % left}% Free"
-        fs_passed = false if left < 10
-      end
-      checks << { text: 'Node File Systems', passed: fs_passed, value: fs_values }
-
-      cpu_values = []
-      cpu_passed = true
-      node_stats['nodes'].each do |name, node|
-        cpu_values << "#{name}: #{node['os']['cpu']['percent']}"
-        cpu_passed = false if node['os']['cpu']['percent'].to_i > 70
-      end
-      checks << { text: 'Node CPU Usage', passed: cpu_passed, value: cpu_values }
-
-      memory_values = []
-      memory_passed = true
-      node_stats['nodes'].each do |name, node|
-        memory_values << "#{name}: #{node['os']['mem']['used_percent']}"
-        memory_passed = false if node['os']['mem']['used_percent'].to_i >= 100
-      end
-      checks << { text: 'Node Memory Usage', passed: memory_passed, value: memory_values }
 
       status checks.find { |c| c[:passed] == false } ? 500 : 200
       haml :'status/check', locals: { title: 'Status Check', checks: checks }
