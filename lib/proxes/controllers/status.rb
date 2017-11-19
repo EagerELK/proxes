a4d7b006232a212ee94db7433ba02efcbc56fe24
Jurgens du Toit <jrgns@jadeit.co.za>
2017-11-19 05:55:29 +0200

    feat: More robust status page, includes node info

--- lib/proxes/controllers/status.rb
+++ lib/proxes/controllers/status.rb
@@ -24,43 +24,78 @@ module ProxES
         checks << { text: 'Cluster Reachable', passed: true, value: health['cluster_name'] }
         checks << { text: 'Cluster Health', passed: health['status'] == 'green', value: health['status'] }
 
-        node_stats = client.nodes.stats metric: 'os,fs,jvm'
+        node_stats = client.nodes.stats
+
+        master_nodes = []
+        data_nodes = []
+        ingestion_nodes = []
+        node_stats['nodes'].values.each do |node|
+          if node['roles']
+            master_nodes << node['name'] if node['roles'].include? 'master'
+            data_nodes << node['name'] if node['roles'].include? 'data'
+            ingestion_nodes << node['name'] if node['roles'].include? 'ingest'
+          elsif node['attributes']
+            master_nodes << node['name'] unless node['attributes']['master'] == 'false'
+            data_nodes << node['name'] unless node['attributes']['data'] == 'false'
+            ingestion_nodes << node['name'] unless node['attributes']['ingest'] == 'false'
+          elsif node['settings']
+            master_nodes << node['name'] unless node['settings']['node']['master'] == 'false'
+            data_nodes << node['name'] unless node['settings']['node']['data'] == 'false'
+            ingestion_nodes << node['name'] unless node['settings']['node']['ingest'] == 'false'
+          end
+        end
+        checks << {
+          text: 'Master Nodes',
+          passed: master_nodes.count > 0,
+          value: master_nodes.count > 0 ? master_nodes.sort : 'None'
+        }
+        checks << {
+          text: 'Data Nodes',
+          passed: data_nodes.count > 0,
+          value: data_nodes.count > 0 ? data_nodes.sort : 'None'
+        }
+        checks << {
+          text: 'Ingestion Nodes',
+          passed: true,
+          value: ingestion_nodes.count > 0 ? ingestion_nodes.sort : 'None'
+        }
 
         jvm_values = []
         jvm_passed = true
-        node_stats['nodes'].each do |name, node|
-          jvm_values << "#{name}: #{node['jvm']['mem']['heap_used_percent']}%"
+        node_stats['nodes'].values.each do |node|
+          jvm_values << "#{node['name']}: #{node['jvm']['mem']['heap_used_percent']}%"
           jvm_passed = false if node['jvm']['mem']['heap_used_percent'] > 85
         end
-        checks << { text: 'Node JVM Heap', passed: jvm_passed, value: jvm_values }
+        checks << { text: 'Node JVM Heap', passed: jvm_passed, value: jvm_values.sort }
 
         fs_values = []
         fs_passed = true
-        node_stats['nodes'].each do |name, node|
-          next if node['attributes']['data'] == 'false'
+        node_stats['nodes'].values.each do |node|
+          next if node['attributes'] && node['attributes']['data'] == 'false'
+          next if node['roles'] && node['roles'].include?('data') == false
           stats = node['fs']['total']
           left = stats['available_in_bytes'] / stats['total_in_bytes'].to_f * 100
-          fs_values << "#{name}: #{'%.02f' % left}% Free"
+          fs_values << "#{node['name']}: #{'%.02f' % left}% Free"
           fs_passed = false if left < 10
         end
-        checks << { text: 'Node File Systems', passed: fs_passed, value: fs_values }
+        checks << { text: 'Node File Systems', passed: fs_passed, value: fs_values.sort }
 
         cpu_values = []
         cpu_passed = true
-        node_stats['nodes'].each do |name, node|
+        node_stats['nodes'].values.each do |node|
           value = (node['os']['cpu_percent'] || node['os']['cpu']['percent'])
-          cpu_values << "#{name}: #{value}"
+          cpu_values << "#{node['name']}: #{value}"
           cpu_passed = false if value.to_i > 70
         end
-        checks << { text: 'Node CPU Usage', passed: cpu_passed, value: cpu_values }
+        checks << { text: 'Node CPU Usage', passed: cpu_passed, value: cpu_values.sort }
 
         memory_values = []
         memory_passed = true
-        node_stats['nodes'].each do |name, node|
-          memory_values << "#{name}: #{node['os']['mem']['used_percent']}"
+        node_stats['nodes'].values.each do |node|
+          memory_values << "#{node['name']}: #{node['os']['mem']['used_percent']}"
           memory_passed = false if node['os']['mem']['used_percent'].to_i >= 100
         end
-        checks << { text: 'Node Memory Usage', passed: memory_passed, value: memory_values }
+        checks << { text: 'Node Memory Usage', passed: memory_passed, value: memory_values.sort }
       rescue Faraday::Error => e
         checks << { text: 'Cluster Reachable', passed: false, value: e.message}
       end
