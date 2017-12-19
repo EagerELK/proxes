46be8b97f1661303411d86b25e57a7eeb0e91c47
Jurgens du Toit <jrgns@jadeit.co.za>
2017-12-19 22:31:35 +0200

    chore: Extend ES cleint to include logger

--- lib/proxes/services/es.rb
+++ lib/proxes/services/es.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 require 'elasticsearch'
+require 'ditty/services/logger'
 
 module ProxES
   module Services
     module ES
       def client
-        @client ||= Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL']
+        @client ||= Elasticsearch::Client.new(
+          url: ENV['ELASTICSEARCH_URL'],
+          transport_options: {
+            ssl: { verify: ENV['SSL_VERIFY_NONE'].to_i != 1 }
+          },
+          logger: Ditty::Services::Logger.instance
+        )
       end
 
       def cluster_health(level = 'cluster')
