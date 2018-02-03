a1212d1486f78ce9694596e8de7a639a804c2ddd
Jurgens du Toit <jrgns@jadeit.co.za>
2018-02-03 15:21:24 +0200

    chore: Fix docker deploy

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-docker login --email=$DOCKER_HUB_EMAIL --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
-docker build -t proxes-os:$TRAVIS_TAG .
+docker -v
+docker login --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
+docker build -t $DOCKER_HUB_USERNAME/proxes-os:latest .
+docker tag $DOCKER_HUB_USERNAME/proxes-os $DOCKER_HUB_USERNAME/proxes-os:$TRAVIS_TAG
+docker push $DOCKER_HUB_USERNAME/proxes-os:latest
 docker push $DOCKER_HUB_USERNAME/proxes-os:$TRAVIS_TAG
