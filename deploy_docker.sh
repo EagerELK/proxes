a3548220f57710a21e359c32ecfb4bb13c08c469
Jurgens du Toit <jrgns@jadeit.co.za>
2018-02-04 14:55:46 +0200

    fix: Push docker image to eagerelk repo

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -2,7 +2,7 @@
 
 docker -v
 docker login --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
-docker build -t $DOCKER_HUB_USERNAME/proxes-os:latest .
-docker tag $DOCKER_HUB_USERNAME/proxes-os $DOCKER_HUB_USERNAME/proxes-os:$TRAVIS_TAG
-docker push $DOCKER_HUB_USERNAME/proxes-os:latest
-docker push $DOCKER_HUB_USERNAME/proxes-os:$TRAVIS_TAG
+docker build -t eagerelk/proxes:latest .
+docker tag eagerelk/proxes eagerelk/proxes:$TRAVIS_TAG
+docker push eagerelk/proxes:latest
+docker push eagerelk/proxes:$TRAVIS_TAG
