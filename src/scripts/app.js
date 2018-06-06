6a9a49eff8df729619533d579b0195d3a5036747
Jurgens du Toit <jrgns@jadeit.co.za>
2018-06-07 00:30:54 +0200

    chore: Update react component / dashboard

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ProxesComponents from 'react-proxes-components/react-proxes-components';
+import Form from 'react-jsonschema-form';
 
-var elasticsearch_url = document.getElementById('react-dashboard').getAttribute('data-elasticsearch-url');
-ReactDOM.render(<ProxesComponents pollInterval="30000" elasticsearch_url={elasticsearch_url}/>, document.getElementById('react-dashboard'));
-
-// ReactDOM.render(
-//     <Health store={new ESStore()}/>,
-//     document.getElementById('indexlist')
-// );
+if (document.getElementById('react-dashboard')) {
+  const elasticsearch_url = document.getElementById('react-dashboard').getAttribute('data-elasticsearch-url');
+  ReactDOM.render(
+    <ProxesComponents pollInterval="30000" elasticsearch_url={elasticsearch_url}/>,
+    document.getElementById('react-dashboard')
+  );
+}
